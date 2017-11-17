99302a585c3a7a29336857b4f1afcca6590549fd
drevil1200 <awurster1@gmail.com>
2017-11-17 09:40:29 -0600

    v1.5 All files split into categories. (i.e. PyAI, PyPlayers, PySetup, __init__, LICENSE)

--- PySetup.py
+++ PySetup.py
@@ -5,6 +5,9 @@
 
 #Note: The developer of Py Tic Tac Toe is not associated with Tic Tac Toe, random or the developers of Creative Commons.
 import random
+from PyPlayers import inputPlayerLetter
+from PyPlayers import getPlayerMove
+from PyAI import getComputerMove
 board = []
 def drawBoard(board):
     #prints out Tic Tac Toe board
@@ -20,15 +23,7 @@ def drawBoard(board):
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
 #drawBoard(board)   
-def inputPlayerLetter():
-    letter = ''
-    while not (letter == 'X' or letter == 'O'):
-        print('Do you want to be X or O?')
-        letter = input().upper()
-    if letter == 'X':
-        return ['X', 'O']
-    else:
-        return ['O', 'X']
+
 
 def whoGoesFirst():
     if random.randint(0,1) == 0:
@@ -63,12 +58,7 @@ def getBoardCopy(board):
 def isSpaceFree(board, move):
     return board[move] == ' '
 
-def getPlayerMove(board):
-    move = ' '
-    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
-        print('What is your next move? (1-9)')
-        move = input()
-    return int(move)
+
 
 def chooseRandomMoveFromList(board, movesList):
     possibleMoves = []
@@ -81,34 +71,15 @@ def chooseRandomMoveFromList(board, movesList):
     else:
         return None
 
-def getComputerMove(board, computerLetter):
-    if computerLetter == 'X':
-        playerLetter = 'O'
-    else:
-        playerLetter = 'X'
-
-    for i in range(1,10):
-        copy = getBoardCopy(board)
-        if isSpaceFree(copy, i):
-            makeMove(copy, playerLetter, i)
-            if isWinner(copy, playerLetter):
-                return i
-
-    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
-    if move != None:
-        return move
-
-    if isSpaceFree(board, 5):
-        return 5
-
-    return chooseRandomMoveFromList(board, [2, 4, 6, 8])
-
 def isBoardFull(board):
     for i in range(1, 10):
         if isSpaceFree(board, i):
             return False
     return True
 
+	
+	
+#main game starts here and loops
 print('Welcome to Tic Tac Toe!')
 turn = ' '
 while True:
