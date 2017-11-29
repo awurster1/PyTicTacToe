250723281859412e2c2e0ed8e948e8c884207e93
DESKTOP-4PSMA19\awurs <awurster1@gmail.com>
2017-11-29 14:34:37 -0600

    Revert "Delete PyAI.py"
    This reverts commit a724bd2938c9d8d2859df5db8b5028afb19afa65.

---
+++ PyAI.py
@@ -0,0 +1,27 @@
+def getComputerMove(board, computerLetter):
+    if computerLetter == 'X':
+        playerLetter = 'O'
+    else:
+        playerLetter = 'X'
+
+    for i in range(1,10):
+        from PySetup import getBoardCopy
+        from PySetup import isSpaceFree
+        from PySetup import getBoardCopy
+        from PySetup import makeMove
+        from PySetup import isWinner
+        from PySetup import chooseRandomMoveFromList
+        copy = getBoardCopy(board)
+        if isSpaceFree(copy, i):
+            makeMove(copy, playerLetter, i)
+            if isWinner(copy, playerLetter):
+                return i
+
+    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
+    if move != None:
+        return move
+
+    if isSpaceFree(board, 5):
+        return 5
+
+    return chooseRandomMoveFromList(board, [2, 4, 6, 8])
